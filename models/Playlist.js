44818e42632012603e0b9cee6a8ec8ecaf3edbcb
mnmkato <katsmamal@gmail.com>
2024-06-08 11:05:05 +0000

    fixed playlist deleting functionality

--- models/Playlist.js
+++ models/Playlist.js
@@ -2,10 +2,15 @@ const mongoose = require('mongoose');
 
 // Define playlist schema
 const playlistSchema = new mongoose.Schema({
+    playlistId:{ type: String, required: true },
     link: String,
     thumbnail: String,
     title: String,
-    channelTitle: String
+    channelTitle: String,
+    deleted: {
+        type: Boolean,
+        default: false
+    }
 });
 
 // Define playlist model
--- routes/index.js
+++ routes/index.js
@@ -11,58 +11,70 @@ const maxResults = 100;
 /* GET home page. */
 router.get('/', async function(req, res, next) {
     const channels = await Channel.find().sort({created_at: -1}).exec();
-    const all_playlists = await Playlist.find().sort({created_at: -1}).exec();
+    const all_playlists = await Playlist.find({deleted:false}).sort({created_at: -1}).exec();
     res.render('index', { 
         title: 'UG Shows server',
         all_playlists: all_playlists,
         channels: channels });
 });
 
-router.get('/api/playlists', async (req, res) => {
-    const all_playlists = await Playlist.find().sort({created_at: -1}).exec();
-    res.json(all_playlists);
-})
-router.post('/api/delete/',async (req, res) => {
-    await Playlist.findByIdAndDelete(req.body.playlistId);
-    res.redirect('/')
-})
-router.post('/api/save', async (req, res) => {
+  router.get('/api/playlists', async (req, res) => {
+      const all_playlists = await Playlist.find({deleted:false}).sort({created_at: -1}).exec();
+      res.json(all_playlists);
+  })
+  router.post('/api/delete/',async (req, res) => {
+      await Playlist.findByIdAndUpdate(req.body.playlistId, { deleted: true });
+      //await Playlist.deleteMany({});
+      res.redirect('/')
+  })
+
+  router.post('/api/save', async (req, res) => {
     try {
-    const channels = await Channel.find().sort({created_at: -1}).exec();
-      const allPlaylists = await Promise.all(channels.map(async (channel) => {
-        const response = await axios.get(`https://youtube.googleapis.com/youtube/v3/playlists`, {
-          params: {
-            part: 'snippet',
-            channelId: channel.channelId,
-            maxResults: maxResults,
-            key: apiKey
-          }
-        });
-        const data = response.data.items.map(item => ({
-          link: `https://www.youtube.com/playlist?list=${item.id}`,
-          thumbnail: item.snippet.thumbnails.medium.url,
-          title: item.snippet.title,
-          channelTitle: item.snippet.channelTitle
+        const channels = await Channel.find().sort({ created_at: -1 }).exec();
+        const allPlaylists = await Promise.all(channels.map(async (channel) => {
+            const response = await axios.get(`https://youtube.googleapis.com/youtube/v3/playlists`, {
+                params: {
+                    part: 'snippet',
+                    channelId: channel.channelId,
+                    maxResults: maxResults,
+                    key: apiKey
+                }
+            });
+
+            const data = response.data.items.map(item => ({
+                playlistId: item.id,
+                link: `https://www.youtube.com/playlist?list=${item.id}`,
+                thumbnail: item.snippet.thumbnails.medium.url,
+                title: item.snippet.title,
+                channelTitle: item.snippet.channelTitle
+            }));
+
+            // Fetch playlists from the database to check for existing and deleted ones
+            const existingPlaylists = await Playlist.find({ 
+                playlistId: { $in: data.map(item => item.playlistId) },
+                deleted: false // Only consider non-deleted playlists
+            });
+
+            // Filter out playlists that are already saved in the database
+            const playlistsToInsert = data.filter(item => 
+                !existingPlaylists.some(playlist => playlist.playlistId === item.playlistId)
+            );
+
+            console.log('Playlists to insert:', playlistsToInsert);
+
+            // Insert only the playlists that are not already in the database
+            if (playlistsToInsert.length > 0) {
+                await Playlist.insertMany(playlistsToInsert);
+            }
         }));
-  
-        // Check if each playlist already exists in the database
-        const existingPlaylists = await Playlist.find({ link: { $in: data.map(item => item.link) } });
-  
-        // Filter out playlists that are not already in the database
-        const playlistsToInsert = data.filter(item => !existingPlaylists.some(playlist => playlist.link === item.link));
-  
-        // Insert only the playlists that are not already in the database
-        if (playlistsToInsert.length > 0) {
-          await Playlist.insertMany(playlistsToInsert);
-        }
-      }));
-  
-      res.redirect("/")
+        
+        res.redirect("/");
     } catch (error) {
-      console.error('There was a problem:', error);
-      res.status(500).json({ error: 'Internal server error' });
+        console.error('There was a problem:', error);
+        res.status(500).json({ error: 'Internal server error' });
     }
-  });
+});
+
   router.post('/channel/delete', async (req, res) => {
     await Channel.findByIdAndDelete(req.body.channelId);
     
